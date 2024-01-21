9deb4f17e883973b2e4601757e50011018988f34
ransescampos <ransescampos@hotmail.com>
2024-01-21 17:11:42 -0400

    Versão Final do Desafio do Felipão

---
+++ index.js
@@ -0,0 +1,29 @@
+// DESAFIO CLASSIFICADOR DE NÍVEL DE HEROI
+
+let heroi = "RANSES DEV"
+let xp = 4000
+let nivel = "NULL"
+
+
+
+
+    if (xp <= 1000){
+        nivel = "Ferro"
+    } else if (xp > 1000 && xp <= 2000){
+        nivel = "Bronze"
+    } else if (xp > 2000 && xp <= 5000){
+        nivel = "Prata"
+    } else if (xp > 5000 && xp <= 7000){
+        nivel = "Ouro"
+    } else if (xp > 7000 && xp <= 8000){
+        nivel = "Platina"
+    } else if (xp > 8000 && xp <= 9000){
+        nivel = "Ascendente"
+    } else if (xp > 9000 && xp <=10000){
+        nivel = "Imortal"
+    } else {
+        nivel = "Radiante"
+    }
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+        
+
